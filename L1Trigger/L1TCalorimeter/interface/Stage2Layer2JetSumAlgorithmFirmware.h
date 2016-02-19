3411eb82a362d4e95c578aea22c25ed7d17d8c8c
Jim <jim.brooke@cern.ch>
2016-02-20 00:41:08 +0100

    clean up code, verified this has no impact on algorithms

--- L1Trigger/L1TCalorimeter/interface/Stage2Layer2JetSumAlgorithmFirmware.h
+++ L1Trigger/L1TCalorimeter/interface/Stage2Layer2JetSumAlgorithmFirmware.h
@@ -27,12 +27,14 @@ namespace l1t {
 			      std::vector<l1t::EtSum> & htsums);
   private:
     CaloParamsHelper* params_;
-    int32_t etSumEtThresholdHwEt_;
-    int32_t etSumEtThresholdHwMet_;
-    int32_t etSumEtaMinEt_;
-    int32_t etSumEtaMaxEt_;
-    int32_t etSumEtaMinMet_;
-    int32_t etSumEtaMaxMet_;
+
+    int32_t htJetEtMin_;
+    int32_t htJetEtaMax_;
+
+    int32_t mhtJetEtMin_;
+    int32_t mhtJetEtaMax_;
+
+    int32_t ietaMax_;
 
   };
 
--- L1Trigger/L1TCalorimeter/src/firmware/Stage2Layer2JetSumAlgorithmFirmwareImp1.cc
+++ L1Trigger/L1TCalorimeter/src/firmware/Stage2Layer2JetSumAlgorithmFirmwareImp1.cc
@@ -12,14 +12,18 @@
 l1t::Stage2Layer2JetSumAlgorithmFirmwareImp1::Stage2Layer2JetSumAlgorithmFirmwareImp1(CaloParamsHelper* params) :
   params_(params)
 {
-  etSumEtThresholdHwEt_ = floor(params_->etSumEtThreshold(1)/params_->jetLsb());
-  etSumEtThresholdHwMet_ = floor(params_->etSumEtThreshold(3)/params_->jetLsb());
 
-  etSumEtaMinEt_ = params_->etSumEtaMin(1);
-  etSumEtaMaxEt_ = params_->etSumEtaMax(1);
- 
-  etSumEtaMinMet_ = params_->etSumEtaMin(3);
-  etSumEtaMaxMet_ = params_->etSumEtaMax(3);
+  htJetEtMin_  = floor(params_->etSumEtThreshold(1)/params_->jetLsb());
+  mhtJetEtMin_ = floor(params_->etSumEtThreshold(3)/params_->jetLsb());
+
+  htJetEtaMax_ = params_->etSumEtaMax(1);
+  if (htJetEtaMax_ > CaloTools::kHFEnd) htJetEtaMax_ = CaloTools::kHFEnd;
+
+  mhtJetEtaMax_ = params_->etSumEtaMax(3);
+  if (mhtJetEtaMax_ > CaloTools::kHFEnd) mhtJetEtaMax_ = CaloTools::kHFEnd;
+
+  ietaMax_ = htJetEtaMax_ > mhtJetEtaMax_ ? htJetEtaMax_ : mhtJetEtaMax_;
+
 }
 
 
@@ -32,56 +36,32 @@ l1t::Stage2Layer2JetSumAlgorithmFirmwareImp1::~Stage2Layer2JetSumAlgorithmFirmwa
 void l1t::Stage2Layer2JetSumAlgorithmFirmwareImp1::processEvent(const std::vector<l1t::Jet> & alljets, std::vector<l1t::EtSum> & htsums) 
 {
 
-  int etaMax = etSumEtaMaxEt_ > etSumEtaMaxMet_ ? etSumEtaMaxEt_ : etSumEtaMaxMet_;
-  //  int etaMin = etSumEtaMinEt_ < etSumEtaMinMet_ ? etSumEtaMinEt_ : etSumEtaMinMet_;
-  int phiMax = CaloTools::kHBHENrPhi;
-  int phiMin = 1;
-  
+
   // etaSide=1 is positive eta, etaSide=-1 is negative eta
   for (int etaSide=1; etaSide>=-1; etaSide-=2) {
 
     int32_t hx(0), hy(0), ht(0);
-    
-    std::vector<int> rings;
-    for (int i=1; i<=etaMax; i++) rings.push_back(i*etaSide);
 
-    // loop over rings    
-    for (unsigned etaIt=0; etaIt<rings.size(); etaIt++) {
+    // loop over jets
+    for (auto jet : alljets) {
 
-      int ieta = rings.at(etaIt);
-
-      int32_t ringHx(0), ringHy(0), ringHt(0); 
-
-      // loop over phi
-      for (int iphi=phiMin; iphi<=phiMax; iphi++) {
+      if (etaSide*jet.hwEta() > 0) {
 	
-        // find the jet at this (eta,phi)
-	l1t::Jet thisJet;
-	bool foundJet = false;
-	for (unsigned jetIt=0; jetIt<alljets.size(); jetIt++) {
-	  if (alljets.at(jetIt).hwEta()==ieta && alljets.at(jetIt).hwPhi()==iphi) {
-	    thisJet = alljets.at(jetIt);
-	    foundJet = true;
-	  }
+	if (abs(jet.hwEta()) <= htJetEtaMax_ &&
+	    jet.hwPt() > htJetEtMin_) {
+	  ht += jet.hwPt();
 	}
-	if (!foundJet) continue;
-	
-	if (thisJet.hwPt()>etSumEtThresholdHwMet_ && abs(thisJet.hwEta())<=etSumEtaMaxMet_) {
-	  ringHx += (int32_t) ( thisJet.hwPt() * std::trunc ( 511. * cos ( 2 * M_PI * (72 - (iphi-1)) / 72.0 ) )) >> 9;
-	  ringHy += (int32_t) ( thisJet.hwPt() * std::trunc ( 511. * sin ( 2 * M_PI * (iphi-1) / 72.0 ) )) >> 9;
+
+	if (abs(jet.hwEta()) <= mhtJetEtaMax_ &&
+	    jet.hwPt() > mhtJetEtMin_) {
+	  hx += (int32_t) ( jet.hwPt() * std::trunc ( 511. * cos ( 2 * M_PI * (72 - (jet.hwPhi()-1)) / 72.0 ) )) >> 9;
+	  hy += (int32_t) ( jet.hwPt() * std::trunc ( 511. * sin ( 2 * M_PI * (jet.hwPhi()-1) / 72.0 ) )) >> 9;
 	}
-	
-	if (thisJet.hwPt()>etSumEtThresholdHwEt_ && abs(thisJet.hwEta())<=etSumEtaMaxEt_) {
-	  ringHt += thisJet.hwPt();
-	}
+
       }
 
-      hx += ringHx;
-      hy += ringHy;
-      ht += ringHt;
-      
     }
-
+    
     math::XYZTLorentzVector p4;
     
     l1t::EtSum htSumHt(p4,l1t::EtSum::EtSumType::kTotalHt,ht,0,0,0);
